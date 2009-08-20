bed609a2526428f3af9c1cb6c0d2368af398dda6
SND\khgreg_cp <SND\khgreg_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2009-08-20 07:24:34 +0000

    Added more tests for ApProxy

--- AttackPointPluginTests/Test_ApProxy.cs
+++ AttackPointPluginTests/Test_ApProxy.cs
@@ -14,13 +14,16 @@ namespace AttackPointPluginTests
 {
     public class Test_ApProxy// : TestBase
     {
-        private string _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private string _path;
         private Mock<ILogger> _logger;
+        private ApMetadata _metadata;
 
         public Test_ApProxy() {
-            // Setup logger
             _logger = new Mock<ILogger>();
             LogManager.Logger = _logger.Object;
+
+            _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _metadata = ApMetadata.LoadMetadata(_path);
         }
 
         [Fact]
@@ -29,15 +32,13 @@ namespace AttackPointPluginTests
             var status = WebExceptionStatus.NameResolutionFailure;
             var wex = new WebException(error, status);
 
-            var request = GetRequest();
+            var request = new Mock<IHttpRequestWrapper>();
             request.Setup(r => r.GetRequestStream()).Throws(wex);
             var cp = new Mock<IConnectionProvider>();
             cp.Setup(x => x.GetRequest(It.IsAny<string>())).Returns(request.Object);
 
             _logger.Setup(x => x.LogMessage(GetLogMessage(status, "/dologin.jsp"), wex)).Verifiable();
-
-            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _path, "user", "password"));
-
+            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _metadata, "user", "password"));
             Assert.Equal(GetErrorMessage(status, error), ex.Message);
             _logger.Verify();
         }
@@ -48,7 +49,8 @@ namespace AttackPointPluginTests
             var status = WebExceptionStatus.ConnectFailure;
             var wex = new WebException(error, status);
 
-            var request = GetRequest();
+            var request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetRequestStream()).Returns(new MemoryStream());
             request.Setup(r => r.GetResponse()).Throws(wex);
 
             var cp = new Mock<IConnectionProvider>();
@@ -56,7 +58,7 @@ namespace AttackPointPluginTests
 
             _logger.Setup(x => x.LogMessage(GetLogMessage(status, "/dologin.jsp"), wex)).Verifiable();
 
-            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _path, "user", "password"));
+            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _metadata, "user", "password"));
 
             Assert.Equal(GetErrorMessage(status, error), ex.Message);
             _logger.Verify();
@@ -71,7 +73,8 @@ namespace AttackPointPluginTests
             response.SetupGet(r => r.StatusCode).Returns(status);
             response.SetupGet(r => r.StatusDescription).Returns(error);
 
-            var request = GetRequest();
+            var request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetRequestStream()).Returns(new MemoryStream());
             request.Setup(r => r.GetResponse()).Returns(response.Object);
 
             var cp = new Mock<IConnectionProvider>();
@@ -80,7 +83,7 @@ namespace AttackPointPluginTests
             _logger.Setup(x => x.LogMessage(GetLogMessage(status, error, "/dologin.jsp"))).Verifiable();
             _logger.Setup(x => x.LogWebResponse("/dologin.jsp", response.Object)).Verifiable();
 
-            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _path, "user", "password"));
+            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _metadata, "user", "password"));
 
             Assert.Equal(GetErrorMessage(status, error), ex.Message);
             _logger.Verify();
@@ -91,9 +94,11 @@ namespace AttackPointPluginTests
             var response = new Mock<IHttpResponseWrapper>();
             response.SetupGet(r => r.StatusCode).Returns(HttpStatusCode.OK);
 
-            var request = GetRequest();
+            var request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetRequestStream()).Returns(new MemoryStream());
             request.Setup(r => r.GetResponse()).Returns(response.Object);
             request.SetupGet(r => r.RequestUri).Returns(new Uri("http://attackpoint.org/dologin.jsp"));
+            request.SetupGet(r => r.Cookies).Returns(new CookieCollection());
 
             var cp = new Mock<IConnectionProvider>();
             cp.Setup(x => x.GetRequest(It.IsAny<string>())).Returns(request.Object);
@@ -101,30 +106,35 @@ namespace AttackPointPluginTests
             _logger.Setup(x => x.LogMessage("Unable to authenticate on AttackPoint. Login cookie not found.")).Verifiable();
             _logger.Setup(x => x.LogWebResponse("/dologin.jsp", response.Object)).Verifiable();
 
-            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _path, "user", "password"));
+            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _metadata, "user", "password"));
 
             Assert.Equal("Unable to authenticate on AttackPoint. See log file for the response.", ex.Message);
             _logger.Verify();
         }
 
-        [Fact(Skip="TODO: Implement")]
+        [Fact]
         public void Connect_ResponseWithLoginCookieWithoutUserId() {
-            //var response = new Mock<IHttpResponseWrapper>();
-            //response.SetupGet(r => r.StatusCode).Returns(HttpStatusCode.OK);
+            var response = new Mock<IHttpResponseWrapper>();
+            response.SetupGet(r => r.StatusCode).Returns(HttpStatusCode.OK);
 
-            //var uri = new Uri("http://attackpoint.org/dologin.jsp");
-            //var request = GetRequest();
-            //request.Setup(r => r.GetResponse()).Returns(response.Object);
-            //request.SetupGet(r => r.RequestUri).Returns(uri);
-            ////request.SetupSet(r => r.CookieContainer = c);
+            var request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetRequestStream()).Returns(new MemoryStream());
+            request.Setup(r => r.GetResponse()).Returns(response.Object);
+            request.SetupGet(r => r.RequestUri).Returns(new Uri("http://attackpoint.org/dologin.jsp"));
+            var cookies = new CookieCollection();
+            var cookieValue = "value-which-does-not-have-colon-in-it";
+            cookies.Add(new Cookie("login", cookieValue));
+            request.SetupGet(r => r.Cookies).Returns(cookies);
 
-            //var cp = new Mock<IConnectionProvider>();
-            //cp.Setup(x => x.GetRequest(It.IsAny<string>())).Returns(request.Object);
-            //_logger.Setup(x => x.LogMessage("Can't find user ID in the cookie: cookievalue")).Verifiable();
+            var cp = new Mock<IConnectionProvider>();
+            cp.Setup(x => x.GetRequest(It.IsAny<string>())).Returns(request.Object);
 
-            //var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _path, "user", "password"));
-            //Assert.Equal("Unable to retrieve AP user ID. Perhaps the format of authentication has been changed. See log file for details.", ex.Message);
-            //_logger.Verify();
+            _logger.Setup(x => x.LogMessage("Can't find user ID in the cookie: " + cookieValue)).Verifiable();
+
+            var ex = Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _metadata, "user", "password"));
+
+            Assert.Equal("Unable to retrieve AP user ID. Perhaps the format of authentication has been changed. See log file for details.", ex.Message);
+            _logger.Verify();
         }
 
         [Fact]
@@ -132,18 +142,30 @@ namespace AttackPointPluginTests
             var response = new Mock<IHttpResponseWrapper>();
             response.SetupGet(r => r.StatusCode).Returns(HttpStatusCode.OK);
 
-            var request = GetRequest();
+            var request = new Mock<IHttpRequestWrapper>();
             request.Setup(r => r.GetResponse()).Returns(response.Object);
             request.SetupGet(r => r.RequestUri).Returns(new Uri("http://attackpoint.org/dologin.jsp"));
             var rstream = new MemoryStream();
             request.Setup(r => r.GetRequestStream()).Returns(rstream);
 
+            var cookies = new CookieCollection();
+            var cookieValue = "1234:auth-ticket";
+            var cookie = new Cookie("login", cookieValue);
+            cookie.Expired = false;
+            cookies.Add(cookie);
+            request.SetupGet(r => r.Cookies).Returns(cookies);
+
             var cp = new Mock<IConnectionProvider>();
             cp.Setup(x => x.GetRequest(It.IsAny<string>())).Returns(request.Object);
 
-            // Login cookie will be null, so the exception will be thrown
-            // I don't care. I am interested in the stuff that happens before that
-            Assert.Throws<ApplicationException>(() => ApProxy.Connect(cp.Object, _path, "user", "password"));
+            var proxy = ApProxy.Connect(cp.Object, _metadata, "greg", "password");
+            Assert.Equal("1234", proxy.UserId);
+            Assert.Equal("greg", proxy.Username);
+            Assert.Equal("password", proxy.Password);
+            Assert.False(proxy.Expired);
+
+            cookie.Expired = true;
+            Assert.True(proxy.Expired);
 
             var bytes = rstream.ToArray();
             request.VerifySet(r => r.Method = "POST");
@@ -151,41 +173,178 @@ namespace AttackPointPluginTests
             request.VerifySet(r => r.ContentLength = 31);
 
             Assert.Equal(31, bytes.Length);
-            Assert.Equal("username=user&password=password", new ASCIIEncoding().GetString(bytes));
+            Assert.Equal("username=greg&password=password", new ASCIIEncoding().GetString(bytes));
         }
 
-        //[Fact]
-        //public void Upload_PostedPayload() {
-        //}
+        [Fact]
+        public void Upload_PostedPayload() {
+            Mock<IConnectionProvider> cp;
+            Mock<IHttpRequestWrapper> request;
+            Mock<IHttpResponseWrapper> response;
 
-        //[Fact]
-        //public void ScrapeData_GetResponseThrowsException() {
-        //}
+            var proxy = GetProxy(out cp, out request, out response);
 
-        //[Fact]
-        //public void ScrapeData_GetResponseStreamThrowsException() {
-        //}
+            var note = new ApNote()
+            {
+                Date = new DateTime(2009, 7, 29),
+                Description = "This is a note",
+                RestingHeartRate = "61"
+            };
 
-        //[Fact]
-        //public void ScrapeData_RetrievedPage() {
-        //}
+            var rstream = new MemoryStream();
+            cp.Setup(c => c.GetRequest("http://www.attackpoint.org/addcomment.jsp")).Returns(request.Object);
+            request.Setup(r => r.GetRequestStream()).Returns(rstream);
+            _logger.SetupGet(x => x.IsDebug).Returns(true);
+            _logger.Setup(x => x.LogWebResponse("/addcomment.jsp", response.Object)).Verifiable();
 
-        private Mock<IHttpRequestWrapper> GetRequest() {
-            var request = new Mock<IHttpRequestWrapper>();
-            request.Setup(r => r.GetRequestStream()).Returns(new MemoryStream());
-            return request;
+            proxy.Upload(note);
+
+            request.VerifySet(r => r.Method = "POST");
+            request.VerifySet(r => r.ContentType = "application/x-www-form-urlencoded");
+            request.VerifySet(r => r.ContentLength = 98);
+
+            var bytes = rstream.ToArray();
+            Assert.Equal(98, bytes.Length);
+            Assert.Equal("session-month=07&session-day=29&session-year=2009&rhr=61&sleep=&weight=&description=This+is+a+note", new ASCIIEncoding().GetString(bytes));
+            _logger.Verify();
+            
         }
 
-        private Mock<IHttpResponseWrapper> GetResponse(Stream stream) {
+        [Fact]
+        public void ScrapeData_GetResponseThrowsException() {
+            Mock<IConnectionProvider> cp;
+            Mock<IHttpRequestWrapper> request;
+            Mock<IHttpResponseWrapper> response;
+
+            var proxy = GetProxy(out cp, out request, out response);
+            var profile = new ApProfile();
+
+            var error = "Connection failure.";
+            var status = WebExceptionStatus.ConnectFailure;
+            var wex = new WebException(error, status);
+
+            request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetResponse()).Throws(wex);
+            cp.Setup(x => x.GetRequest("http://www.attackpoint.org/jsonactivitytypes.jsp")).Returns(request.Object);
+
+            _logger.Setup(x => x.LogMessage(GetLogMessage(status, "/jsonactivitytypes.jsp"), wex)).Verifiable();
+            var ex = Assert.Throws<ApplicationException>(() => proxy.ScrapeApData(profile));
+            Assert.Equal(GetErrorMessage(status, error), ex.Message);
+            _logger.Verify();
+        }
+
+        [Fact]
+        public void ScrapeData_ResponseWithNotOkStatus() {
+            Mock<IConnectionProvider> cp;
+            Mock<IHttpRequestWrapper> request;
+            Mock<IHttpResponseWrapper> response;
+
+            var proxy = GetProxy(out cp, out request, out response);
+            var profile = new ApProfile();
+
+            var error = "Resource not found";
+            var status = HttpStatusCode.NotFound;
+
+            response = new Mock<IHttpResponseWrapper>();
+            response.SetupGet(r => r.StatusCode).Returns(status);
+            response.SetupGet(r => r.StatusDescription).Returns(error);
+
+            request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetResponse()).Returns(response.Object);
+            cp.Setup(x => x.GetRequest("http://www.attackpoint.org/jsonactivitytypes.jsp")).Returns(request.Object);
+
+            _logger.Setup(x => x.LogMessage(GetLogMessage(status, error, "/jsonactivitytypes.jsp"))).Verifiable();
+            _logger.Setup(x => x.LogWebResponse("/jsonactivitytypes.jsp", response.Object)).Verifiable();
+            var ex = Assert.Throws<ApplicationException>(() => proxy.ScrapeApData(profile));
+            Assert.Equal(GetErrorMessage(status, error), ex.Message);
+            _logger.Verify();
+        }
+
+        [Fact]
+        public void ScrapeData_GetResponseStreamThrowsException() {
+            Mock<IConnectionProvider> cp;
+            Mock<IHttpRequestWrapper> request;
+            Mock<IHttpResponseWrapper> response;
+
+            var proxy = GetProxy(out cp, out request, out response);
+            var profile = new ApProfile();
+
+            var error = "Not found.";
+            var status = WebExceptionStatus.ProtocolError;
+            var wex = new WebException(error, status);
+
+            response = new Mock<IHttpResponseWrapper>();
+            response.Setup(r => r.GetResponseStream()).Throws(wex);
+            response.SetupGet(r => r.StatusCode).Returns(HttpStatusCode.OK);
+
+            request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetResponse()).Returns(response.Object);
+            cp.Setup(x => x.GetRequest("http://www.attackpoint.org/jsonactivitytypes.jsp")).Returns(request.Object);
+
+            _logger.Setup(x => x.LogMessage(GetLogMessage(status, "/jsonactivitytypes.jsp"), wex)).Verifiable();
+            var ex = Assert.Throws<ApplicationException>(() => proxy.ScrapeApData(profile));
+            Assert.Equal(GetErrorMessage(status, error), ex.Message);
+            _logger.Verify();
+        }
+
+        [Fact]
+        public void ScrapeData_RetrievedPage() {
+            Mock<IConnectionProvider> cp;
+            Mock<IHttpRequestWrapper> request;
+            Mock<IHttpResponseWrapper> response;
+
+            var proxy = GetProxy(out cp, out request, out response);
+            var profile = new ApProfile();
+
+            Setup(cp, "jsonactivitytypes.jsp", "activities.json");
+            Setup(cp, "jsonshoes.jsp", "shoes.json");
+            Setup(cp, "usermenu.jsp", "usersettings.htm");
+
+            proxy.ScrapeApData(profile);
+
+            Assert.True(profile.AdvancedFeaturesEnabledSpecified);
+            Assert.True(profile.AdvancedFeaturesEnabled);
+            Assert.Equal(3, profile.Activities.Count);
+            Assert.Equal(3, profile.Shoes.Count);
+        }
+
+        private void Setup(Mock<IConnectionProvider> cp, string url, string filename) {
             var response = new Mock<IHttpResponseWrapper>();
+            response.SetupGet(r => r.StatusCode).Returns(HttpStatusCode.OK);
+            var stream = new FileStream(Path.Combine(_path, filename), FileMode.Open);
             response.Setup(r => r.GetResponseStream()).Returns(stream);
-            return response;
+
+            var request = new Mock<IHttpRequestWrapper>();
+            request.Setup(r => r.GetResponse()).Returns(response.Object);
+            cp.Setup(x => x.GetRequest("http://www.attackpoint.org/" + url)).Returns(request.Object);
         }
 
-        private Mock<IConnectionProvider> GetConnectionProvider(string url) {
-            var cp = new Mock<IConnectionProvider>();
-            cp.Setup(x => x.GetRequest(url)).Returns(GetRequest().Object);
-            return cp;
+        private ApProxy GetProxy(out Mock<IHttpRequestWrapper> request, out Mock<IHttpResponseWrapper> response) {
+            Mock<IConnectionProvider> cp;
+            return GetProxy(out cp, out request, out response);
+        }
+
+        private ApProxy GetProxy(out Mock<IConnectionProvider> cp, out Mock<IHttpRequestWrapper> request, out Mock<IHttpResponseWrapper> response) {
+            cp = new Mock<IConnectionProvider>();
+            response = new Mock<IHttpResponseWrapper>();
+            request = new Mock<IHttpRequestWrapper>();
+            response.SetupGet(r => r.StatusCode).Returns(HttpStatusCode.OK);
+
+            request.Setup(r => r.GetResponse()).Returns(response.Object);
+            request.SetupGet(r => r.RequestUri).Returns(new Uri("http://www.attackpoint.org/dologin.jsp"));
+            var rstream = new MemoryStream();
+            request.Setup(r => r.GetRequestStream()).Returns(rstream);
+
+            var cookies = new CookieCollection();
+            var cookieValue = "1234:auth-ticket";
+            var cookie = new Cookie("login", cookieValue);
+            cookie.Expired = false;
+            cookies.Add(cookie);
+            request.SetupGet(r => r.Cookies).Returns(cookies);
+
+            cp.Setup(x => x.GetRequest("http://www.attackpoint.org/dologin.jsp")).Returns(request.Object);
+
+            return ApProxy.Connect(cp.Object, _metadata, "greg", "password");
         }
 
         private static string GetErrorMessage(WebExceptionStatus status, string error) {
@@ -203,5 +362,6 @@ namespace AttackPointPluginTests
         private static string GetLogMessage(HttpStatusCode status, string error, string url) {
             return string.Format("Request to URL '{2}' failed with status {0} ({1}).", status, error, url);
         }
+
     }
 }
